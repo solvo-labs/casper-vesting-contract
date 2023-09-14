bd2021e8898f89eecc2538d7bb8073dbbdab89d7
afarukcali <afarukcali@gmail.com>
2023-09-14 14:47:48 +0300

    Error messages extended.

--- error.rs
+++ error.rs
@@ -5,6 +5,9 @@ use casper_types::ApiError;
 pub enum Error {
     FatalError = 0,
     VestingStartError = 1,
+    UserError = 2,
+    UnsufficentBalance = 3,
+    AdminError = 4
 }
 
 impl From<Error> for ApiError {
